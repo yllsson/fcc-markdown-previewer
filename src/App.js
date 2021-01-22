4b4a135f190aba5f95a23c6a87f2f2cac6cc50f2
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-01-22 18:12:10 +0000

    adding the textarea and preliminary preview div

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
+// fcc stories still to be fulfilled
+// User Story #3: When I enter text into the #editor element, the #preview element is updated as I type to display the content of the textarea.
+// User Story #4: When I enter GitHub flavored markdown into the #editor element, the text is rendered as HTML in the #preview element as I type (HINT: You don't need to parse Markdown yourself - you can import the Marked library for this: https://cdnjs.com/libraries/marked).
+// User Story #5: When my markdown previewer first loads, the default text in the #editor field should contain valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text.
+// User Story #6: When my markdown previewer first loads, the default markdown in the #editor field should be rendered as HTML in the #preview element.
+// Optional Bonus (you do not need to make this test pass): My markdown previewer interprets carriage returns and renders them as br (line break) elements.
+
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id='editor'>Textarea</textarea>
+      <div id='preview'>Preview</div>
     </div>
   );
 }
