6b84306a276131d7b6a595f956ebc6eeca01a1e4
Ylva-Thorsen <ylva.e.thorsen@gmail.com>
2021-02-04 22:53:34 +0000

    amend initial text and add minor style to blockquotes

--- src/App.js
+++ src/App.js
@@ -11,15 +11,21 @@ import Preview from './components/Preview';
 function App() {
   const marked = require('marked');
   marked.setOptions({
-    breaks: true
+    breaks: true,
+    gfm: true
   });
 
   const initialText = `
-Here's how to use this previewer:
-===========================
+##### *Write your markdown in the editor to the left and it'll show up in the previewer on the right!*
+
 <br>
 
-The hash symbols create headers according to the amount of symbols:
+Here are some of the things you can do:
+=====
+<br>
+
+## Create a header
+The hash symbols create headers according to the amount of hash symbols added:
 # H1 
 ## H2 
 ### H3 
@@ -28,17 +34,22 @@ The hash symbols create headers according to the amount of symbols:
 ###### H6 is the max
 <br>
 
-#### This is how you add a link
+## Add a link
 
 You add your [text within the brackets](https://google.com/) and your url within the parentheses. 
 This particular one leads to google. :)
 <br>
 
-#### This is how you add inline code
+## Add an image
+Use an exclamation mark, followed by your alt text wrapped in brackets, followed by the link to your image wrapped in parentheses!
+!["A waving polar bear"](https://images.unsplash.com/photo-1589656966895-2f33e7653819?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)
+<br>
+
+## Add inline code
 Put the code within backticks: \`console.log("Hello World")\` 
 <br>
 
-#### This is how you add a full block of code
+## Add a full block of code
 If you want a full block of code you use 3 backticks before and after your block.
 \`\`\`
 var text = "Hello World!"
@@ -46,36 +57,37 @@ console.log(text)
 \`\`\`
 <br>
 
-#### This is how you add an unordered list
+## Create lists! 
+An unordered list is made with simple hyphens in front of each list item
 - a list item
 - another list item
 - a third list item
 
 <br>
 
-#### This is how you add an ordered list
+While an ordered list is created by writing numbers
 1. a list item
 2. another list item
 3. a third list item
 
 <br>
 
-#### This is how you add a blockquote 
-> a blockquote 
-> more lines of the quote
-> it's not a very lengthy quote
+## Add a blockquote 
+> A blockquote is created using "greater than" signs at the start of your sentence!
+> Here's another line of the quote
+> ... It's not a very lengthy quote...
+
 <br>
 
-#### This is how you add an image
-![alt text](https://images.unsplash.com/photo-1589656966895-2f33e7653819?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80 "A waving polar bear")
+## And last but not least
+You add *cursive text* using one asterisk on each side of the selection and **bolded text** using two asterisks.
 <br>
 
-#### And last but not least
-You add *cursive text* using one asterisk on each side of the text and **bolded text** using two asterisks.`;
+## Now it's your turn! Go on - give it a go!
+`;
 
   const [editValue, setEditValue] = useState(initialText);
 
-  console.log(marked(editValue));
   return (
     <div className='App'>
       <Header />
